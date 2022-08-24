70a3699cf53e4f7d1ee7ab4e89ce56d79d5b8801
Anabiga <110169510+Anabiga@users.noreply.github.com>
2022-08-24 09:31:45 +0000

    Add files via upload

---
+++ models/users.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const { Schema,model } = mongoose;
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    phone: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    location: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
